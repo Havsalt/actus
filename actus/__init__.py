de231585655db8fbb2511345df4763d4c147119f
Havsalt <knutolaihelgesen@gmail.com>
2024-06-08 22:01:32 +0200

    Refactored into LogSection class

--- actus/__init__.py
+++ actus/__init__.py
@@ -8,130 +8,27 @@ Includes:
 - `info`
 - `warn`
 - `error`
+- `LogSection`
 - `Theme`
 - `set_theme`
 - `get_theme`
 """
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __all__ = [
     "info",
     "warn",
     "error",
+    "LogSection",
     "Theme",
     "set_theme",
     "get_theme"
 ]
 
-import re as _re
-import sys as _sys
-
-import colex as _colex
-
-from ._theme import Theme
-from ._annotations import (
-    FileLike as _FileLike
-)
+from ._log_section import LogSection
+from ._theme import Theme, set_theme, get_theme
 
 
-def set_theme(new_theme: Theme) -> None:
-    """Sets the global theme
-
-    Args:
-        new_theme (Theme): new global theme
-    """
-    global _theme
-    _theme = new_theme
-
-_theme: Theme = Theme() # global theme
-
-def get_theme() -> Theme: # 
-    """Returns a unique copy of the global theme
-
-    Returns:
-        Theme: unique copy of current global theme
-    """
-    return Theme(
-        info=_theme.info,
-        warn=_theme.warn,
-        error=_theme.error
-    )
-
-
-_BRACKET_CONTENT_PATTERN = _re.compile(r"\[([^]]+)\]")
-
-def _replace_match(match: _re.Match[str]) -> str:
-    return _theme.highlight + match.group(1) + _theme.text
-
-def _highlight(string: str) -> str:
-    return (
-        _theme.text
-        + _BRACKET_CONTENT_PATTERN.sub(_replace_match, string)
-        + _colex.RESET
-    )
-
-
-def info(
-    *values: str,
-    sep: str = " ",
-    end: str = "\n",
-    file: _FileLike[str] | None = None,
-    flush: bool = True
-) -> None:
-    message = (
-        _colex.colorize("[Info]", _theme.info)
-        + " "
-        + _highlight(sep.join(values))
-        + end
-    )
-    if file is not None:
-        file.write(message)
-        if flush:
-            file.flush()
-    _sys.stdout.write(message)
-    if flush:
-        _sys.stdout.flush()
-
-
-def warn(
-    *values: str,
-    sep: str = " ",
-    end: str = "\n",
-    file: _FileLike[str] | None = None,
-    flush: bool = True
-) -> None:
-    message = (
-        _colex.colorize("[Warning]", _theme.warn)
-        + " "
-        + _highlight(sep.join(values))
-        + end
-    )
-    if file is not None:
-        file.write(message)
-        if flush:
-            file.flush()
-    _sys.stdout.write(message)
-    if flush:
-        _sys.stdout.flush()
-
-
-def error(
-    *values: str,
-    sep: str = " ",
-    end: str = "\n",
-    file: _FileLike[str] | None = None,
-    flush: bool = True
-) -> None:
-    message = (
-        _colex.colorize("[Error]", _theme.error)
-        + " "
-        + _highlight(sep.join(values))
-        + end
-    )
-    if file is not None:
-        file.write(message)
-        if flush:
-            file.flush()
-    _sys.stdout.write(message)
-    if flush:
-        _sys.stdout.flush()
+info = LogSection("Info", theme_key="info")
+warn = LogSection("Warning", theme_key="warn")
+error = LogSection("Error", theme_key="error")
