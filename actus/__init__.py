00022f8752de15ea8cb16489de033cad9655bae5
Havsalt <knutolaihelgesen@gmail.com>
2024-06-08 13:25:24 +0200

    Added colors

--- actus/__init__.py
+++ actus/__init__.py
@@ -1,37 +1,140 @@
+"""
+Actus
+-----
 
-__version__ = "0.1.0"
+A logging library for python development
+
+Includes:
+- `info`
+- `warn`
+- `error`
+- `Theme`
+- `set_theme`
+- `get_theme`
+"""
+
+__version__ = "0.2.0"
 __all__ = [
     "info",
     "warn",
-    "error"
+    "error",
+    "Theme",
+    "set_theme",
+    "get_theme"
 ]
 
 import sys as _sys
-from typing import TypeVar as _TypeVar, Protocol as _Protocol
+from string import Formatter as _Formatter
+
+import colex as _colex
+
+from ._theme import Theme
+from ._annotations import (
+    FileLike as _FileLike
+)
 
 
-_T_contra = _TypeVar("_T_contra", contravariant=True)
+def set_theme(new_theme: Theme) -> None:
+    """Sets the global theme
 
-class _SupportsWrite(_Protocol[_T_contra]):
-    def write(self, stream: _T_contra, /) -> object: ...
+    Args:
+        new_theme (Theme): new global theme
+    """
+    global _theme
+    _theme = new_theme
+
+_theme: Theme = Theme() # global theme
+
+def get_theme() -> Theme: # 
+    """Returns a unique copy of the global theme
+
+    Returns:
+        Theme: unique copy of current global theme
+    """
+    return Theme(
+        info=_theme.info,
+        warn=_theme.warn,
+        error=_theme.error
+    )
 
 
-def info(*message_segments: str, sep: str = " ", end: str = "\n", file: _SupportsWrite[str] | None = None) -> None:
-    message = "[Info] " + sep.join(message_segments) + end
+def _highlight(string: str) -> str:
+    colored = _colex.colorize(string, _theme.text)
+    keys = [
+        result[1]
+        for result in _Formatter().parse(string)
+        if result[1]
+    ]
+    highlighted_values = [
+        _theme.highlight + key + _theme.text
+        for key in keys
+    ]
+    pairs = dict(zip(keys, highlighted_values))
+    highlighted = colored.format_map(pairs)
+    return highlighted
+
+
+def info(
+    *values: str,
+    sep: str = " ",
+    end: str = "\n",
+    file: _FileLike[str] | None = None,
+    flush: bool = True
+) -> None:
+    message = (
+        _colex.colorize("[Info]", _theme.info)
+        + " "
+        + _highlight(sep.join(values))
+        + end
+    )
     if file is not None:
         file.write(message)
+        if flush:
+            file.flush()
     _sys.stdout.write(message)
+    if flush:
+        _sys.stdout.flush()
 
 
-def warn(*message_segments: str, sep: str = " ", end: str = "\n", file: _SupportsWrite[str] | None = None) -> None:
-    message = "[Warning] " + sep.join(message_segments) + end
+def warn(
+    *values: str,
+    sep: str = " ",
+    end: str = "\n",
+    file: _FileLike[str] | None = None,
+    flush: bool = True
+) -> None:
+    message = (
+        _colex.colorize("[Warning]", _theme.warn)
+        + " "
+        + _highlight(sep.join(values))
+        + end
+    )
     if file is not None:
         file.write(message)
+        if flush:
+            file.flush()
     _sys.stdout.write(message)
+    if flush:
+        _sys.stdout.flush()
 
 
-def error(*message_segments: str, sep: str = " ", end: str = "\n", file: _SupportsWrite[str] | None = None) -> None:
-    message = "[Error] " + sep.join(message_segments) + end
+def error(
+    *values: str,
+    sep: str = " ",
+    end: str = "\n",
+    file: _FileLike[str] | None = None,
+    flush: bool = True
+) -> None:
+    message = (
+        _colex.colorize("[Error]", _theme.error)
+        + " "
+        + _highlight(sep.join(values))
+        + end
+    )
     if file is not None:
         file.write(message)
+        if flush:
+            file.flush()
     _sys.stdout.write(message)
+    if flush:
+        _sys.stdout.flush()
